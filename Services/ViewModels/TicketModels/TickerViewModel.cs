7d38621c8fb6a0f3a2f12c91ef0a81d99be3be93
Minh Quang Trang <quangtm0152@gmail.com>
2023-11-04 00:10:39 +0700

    AddView

--- Services/ViewModels/TicketModels/TickerViewModel.cs
+++ Services/ViewModels/TicketModels/TickerViewModel.cs
@@ -7,6 +7,7 @@ public class TicketViewModel
     public Guid TripId { get; set; } = default!;
     public TripViewModel Trip { get; set; } = default!;
     public double Price { get; set; } = default!;
+    public int Quantity { get; set; } = default!;
     public string Name { get; set; } = default!;
     public Guid OrderId { get; set; } = default!;
 }
